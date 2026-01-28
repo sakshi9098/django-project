437f823f1c0d0462d23af320db6733b711a75490
Sakshidevkar2005 <devkarsakshi20@gmail.com>
2026-01-28 14:31:45 +0530

    Added Django Interview Q&A page using Bootstrap Accordion

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, qa_view
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
+    path('qa/', qa_view, name='qa'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.views.generic import ListView
+from django.shortcuts import render
 from .models import Post
+
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'all_posts'
+
+
+def qa_view(request):
+    return render(request, 'blog/qa.html')
